b7a4e6d1dbbaf670353cabcd360f19cfb121c139
Jafrancisco15 <147249691+Jafrancisco15@users.noreply.github.com>
2025-08-30 16:18:15 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,13 @@
 // vite.config.js
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react' // o '@vitejs/plugin-react-swc'
+import react from '@vitejs/plugin-react'  // o '@vitejs/plugin-react-swc'
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
-    dedupe: ['react', 'react-dom'],
+    dedupe: ['react', 'react-dom'],   // 👈 fuerza una sola copia
   },
   optimizeDeps: {
-    include: ['react', 'react-dom'],
+    include: ['react', 'react-dom'],  // 👈 pre-bundle único
   },
 })
