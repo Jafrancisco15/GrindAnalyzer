733a69f43bb765d6f44f0b18f2735e15986b1dbe
Jafrancisco15 <147249691+Jafrancisco15@users.noreply.github.com>
2025-08-30 15:19:30 -0400

    r

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,6 @@
-
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
-  theme: {
-    extend: {
-      colors: {
-        ink: "#0b0b0b",
-        accent: "#FFD000",
-      },
-      dropShadow: {
-        logo: "0 1px 1px rgba(0,0,0,0.9), 0 2px 6px rgba(0,0,0,0.7)"
-      }
-    },
-  },
+  theme: { extend: {} },
   plugins: [],
 }
